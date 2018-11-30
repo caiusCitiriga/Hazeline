f67fb005ff32a800f1b2467f9bcefbf7cc1ca7d4
Caius Citiriga <nbkmaxine@gmail.com>
2018-11-30 12:38:29 +0100

    Created readme

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,3 +1,37 @@
-window.onload = () => {
+import { Hazeline } from 'hazeline';
 
+window.onload = () => {
+    const haze = new Hazeline();
+    haze.addSection({
+        id: 'test',
+        steps: [
+            {
+                elementSelector: '#input-1',
+                text: 'This is an awesome input box',
+                dynamicOptions: {
+                    overlay: {
+                        closeBtnText: 'Quit',
+                        overlayCSS: {
+                            background: 'rgba(255, 255, 255, .85)'
+                        }
+                    },
+                    lightbox: {
+                        nextBtnText: '>',
+                        prevBtnText: '<',
+                        lightboxNextBtnCSS: {
+                            border: 'none'
+                        },
+                        lightboxPrevBtnCSS: {
+                            border: 'none'
+                        },
+                        lightboxWrapperCSS: {
+                            boxShadow: '0px 3px 12px -5px #333'
+                        }
+                    }
+                }
+            },
+        ],
+    });
+
+    haze.runTutorial('test');
 }
